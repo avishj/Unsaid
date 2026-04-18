3f62e7c2cced8d21235935d85616b6ecc7bdfcd0
Avish Jha <avish.j@pm.me>
2026-04-18 11:33:39 -0400

    ui: orb responsiveness
    Signed-off-by: Avish Jha <avish.j@pm.me>

--- frontend/src/components/canvas/Scene.tsx
+++ frontend/src/components/canvas/Scene.tsx
@@ -1,12 +1,23 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useState, useEffect } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { EffectComposer, Bloom, Noise } from '@react-three/postprocessing';
 import { BreathingOrb } from './BreathingOrb';
 
 export const Scene: React.FC = () => {
+  const [fov, setFov] = useState(45);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setFov(window.innerWidth < 768 ? 60 : 45);
+    };
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <div className="fixed inset-0 -z-10 bg-background">
-      <Canvas camera={{ position: [0, 0, 5], fov: 45 }}>
+      <Canvas camera={{ position: [0, 0, 5], fov }}>
         <color attach="background" args={['#050505']} />
         <ambientLight intensity={0.5} />
         <pointLight position={[10, 10, 10]} />
